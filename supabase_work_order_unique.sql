cf81694fe22698cb026dd4e8960121c3cf58eb8c
tanka2c7121-alt <tanka2c7121@gmail.com>
2026-06-01 10:36:23 +0900

    작명 중복 저장 방지 추가

--- supabase_work_order_unique.sql
+++ supabase_work_order_unique.sql
@@ -1,3 +1,9 @@
+delete from work_details a
+using work_details b
+where a.work_name = b.work_name
+  and a.line_no = b.line_no
+  and a.id < b.id;
+
 create unique index if not exists work_orders_work_name_unique
 on work_orders (work_name);
 
